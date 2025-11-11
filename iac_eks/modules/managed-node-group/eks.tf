1fb87787a600d4c793a59b42098553473df7894d
carina-pereira-devops <devops79@yahoo.com>
2025-11-10 21:50:55 -0300

    Ajuste no Familia EC2 - t3.micro

--- iac_eks/modules/managed-node-group/eks.tf
+++ iac_eks/modules/managed-node-group/eks.tf
@@ -2,7 +2,7 @@ resource "aws_eks_node_group" "eks_managed_node_group" {
   cluster_name    = var.cluster_name
   node_group_name = "${var.project_name}-nodegroup"
   node_role_arn   = aws_iam_role.eks_eks_role.arn
-  instance_types  = ["t2.micro"]
+  instance_types  = ["t3.micro"]
   subnet_ids = [
     #Subnets privedas
     var.subnet_private_1a,
