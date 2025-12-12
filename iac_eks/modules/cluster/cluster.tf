e1400a786b4286444e2e4e63d18c0dae98b38a21
carina-pereira-devops <devops79@yahoo.com>
2025-12-12 19:12:58 -0300

    Configurando permissões para deploy vai Helm

--- iac_eks/modules/cluster/cluster.tf
+++ iac_eks/modules/cluster/cluster.tf
@@ -26,3 +26,21 @@ resource "aws_eks_cluster" "eks_cluster" {
   )
 }
 
+# Configuração explícita com as permissoões da Role de Criação do Cluster
+resource "aws_eks_access_entry" "github_actions" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  principal_arn = "arn:aws:iam::749000351410:role/github-carina-devops-pipe"
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "github_actions_admin" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  principal_arn = "arn:aws:iam::749000351410:role/github-carina-devops-pipe"
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
+
