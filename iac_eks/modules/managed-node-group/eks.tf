652cfb2a3ea69ba8106e68afb4914dec20227a5c
carina-pereira-devops <devops79@yahoo.com>
2025-12-12 22:13:32 -0300

    Ajuste na quantidade de réplicas do EKS

--- iac_eks/modules/managed-node-group/eks.tf
+++ iac_eks/modules/managed-node-group/eks.tf
@@ -19,9 +19,9 @@ resource "aws_eks_node_group" "eks_managed_node_group" {
 # Obs.: Prometheus Statefulset, vai subir em todos os Nodes, o que pode consumir recursos desnecessários
   scaling_config {
 
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
+    desired_size = 8
+    max_size     = 6
+    min_size     = 2
   }
 
   depends_on = [
