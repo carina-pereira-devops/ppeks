22de152e9132d226043d2328f458982b8ff4e8eb
carina-pereira-devops <devops79@yahoo.com>
2025-12-17 20:10:00 -0300

    Ajustes nas configurações de RDS

--- rds/outputs.tf
+++ rds/outputs.tf
@@ -1,9 +1,9 @@
 output "rds_endpoint" {
-  description = "Endpoint DNS do RDS"
-  value       = aws_db_instance.this.address
+  description = "Endpoint do RDS Postgres"
+  value       = aws_db_instance.postgres.address
 }
 
 output "rds_db_name" {
-  description = "Nome do database"
-  value       = aws_db_instance.this.name
+  description = "Nome do banco no RDS"
+  value       = aws_db_instance.postgres.db_name
 }
