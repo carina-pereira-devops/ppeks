b09499f043ba3282bd0e394fb82deb16542fcb46
carina-pereira-devops <devops79@yahoo.com>
2025-12-17 20:24:31 -0300

    Ajustes nas declarações das vars do RDS

--- rds/variables.tf
+++ rds/variables.tf
@@ -5,11 +5,13 @@ variable "region" {
 
 variable "vpc_id" {
   type        = string
+  default     = vpc-01a096d1a354420f6 # Automatizar essa info, para não ter Id dinâmico
   description = "VPC onde o RDS será criado"
 }
 
 variable "private_subnets" {
   type        = list(string)
+  default     = ["subnet-0ef720bd932deb291", "subnet-070b36a9167e55fd9"] # Automatizar essa info, para não ter Id dinâmico
   description = "Subnets privadas para o db subnet group"
 }
 
