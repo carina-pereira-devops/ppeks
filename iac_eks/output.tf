f9be6e9e6bf837a4fc6d30ebb8a86e699fbd792f
carina-pereira-devops <devops79@yahoo.com>
2025-12-18 21:45:08 -0300

    Ajuste no Outputs

--- iac_eks/output.tf
+++ iac_eks/output.tf
@@ -1,17 +1,17 @@
 # Alimentar RDS
 
 output "eks_vpc_id" {
-  value = aws_vpc.eks_vpc.id
+  value = module.network.vpc_id
 }
 
 output "eks_private_subnets" {
   description = "Subnets privadas usadas pelo EKS/RDS"
   value = [
-    aws_subnet.eks_subnet_private_1a.id,
-    aws_subnet.eks_subnet_private_1b.id,
+    module.network.subnet_priv_1a,
+    module.network.subnet_priv_1b,
   ]
 }
 
 output "project_name" {
-  value = var.project_name
+  value = module.network.project_name
 }
